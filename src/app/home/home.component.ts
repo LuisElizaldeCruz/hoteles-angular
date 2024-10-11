832b6fd0b431c21a3a376967e4c8ad97d1b46290
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-10-11 00:17:48 -0600

    se agrego la funcion de busqueda

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,8 +15,20 @@ export class HomeComponent {
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
   
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
